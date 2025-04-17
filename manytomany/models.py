403b053b814828db6475c45c0aca19581e54724c
Anton Vasilyev <ant0nvas1990@yandex.ru>
2025-04-17 18:31:56 +0400

    Результаты урока 3.4

---
+++ manytomany/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    courses = models.ManyToManyField(Course)
+# Create your models here.

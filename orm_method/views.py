e901275ba398ef02ba1ec4c2d66fc599622acb56
Anton Vasilyev <ant0nvas1990@yandex.ru>
2025-04-22 17:13:43 +0400

    Результаты урока 3.6

--- orm_method/views.py
+++ orm_method/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from orm_method.models import Worker
+
+
+def index(request):
+    people = Worker.objects.all()
+    return render(request, 'index.html', {'people': people})
+
+
 
 # Create your views here.
